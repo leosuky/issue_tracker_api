d24485876b22d8519a8f24f74cdaae04901ea484
Sunkanmi <leosuky99@gmail.com>
2022-08-16 19:00:06 +0100

    created custom user model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,48 @@
-# from django.db import models
+"""
+Database Models
+----------------------------------------------
+This works because we update the AUTH_USER_MODEL
+setting in our settings.py file.
+"""
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """Overriding the base User Manager"""
+
+    def create_user(self, email, password=None, **kwargs):
+        """Create, Save and Return New User"""
+        if not email:
+            raise ValueError('User must have an Email Address')
+        user = self.model(email=self.normalize_email(email), **kwargs)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create and return a new superuser."""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Overriding the default user model in django"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    """Assign manager to User Class"""
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
